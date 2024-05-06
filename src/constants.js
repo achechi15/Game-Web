b6205f3ec9bf7eaa087f2728be72e7941c13593d
achechi15 <142111179+achechi15@users.noreply.github.com>
2024-05-06 21:35:24 +0200

    v0.1

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC`,
+    'cs-degree': 'This is my future double degree of CS and Econonomics',
+    'sofa-table': `The sofa look super comfortable`,
+    tv: 'Doraemon is being displayed on the tv',
+    bed: 'Go to sleep',
+    resume: `Contact me: amoncal@upv.edu.es`,
+    projects: `My future projects are in my <a href="https://github.com/achechi15">Github</a>`,
+    library: `There is my favourite book: Also Sprach Zarathustra`,
+    exit: 'go kill yourself'
+
+}
